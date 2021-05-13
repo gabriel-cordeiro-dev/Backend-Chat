c077d3a11ae16e5074dcea20867c8a3bedb08adb
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2021-05-13 12:39:55 -0300

    Inicio do projeto
    Criação do banco, migrations, rotas, repositories e entities.
    
    Todos criados com sucesso e rodando

---
+++ src/database/migrations/1620916348453-CreateSettings.ts
@@ -0,0 +1,47 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSettings1620916348453 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "settings",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "username",
+                        type: "varchar",
+                    },
+                    {
+                        name: "chat",
+                        type: "boolean",
+                        default: true
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("settings")
+    }
+
+}
+
+//sempre que rodar os comandos do migrations como:
+//yarn typeorm migration:run => roda tudo oq estiver aqui dentro desta pasta
+//yarn typeorm migration:revert => reverte tudo oq foi feito
